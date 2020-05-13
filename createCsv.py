b4cd99ff0335e549e52a3ecf4e8f46332aaf416a
Alllessia <41387340+Alllessia@users.noreply.github.com>
2020-05-13 17:37:28 +0200

    Select all the data needed

--- createCsv.py
+++ createCsv.py
@@ -3,17 +3,14 @@ import pandas
 import glob
 
 logAngles = pandas.DataFrame(pandas.read_csv('soudLocalizer_dumper/logAngles/data.log', sep=' ', header=None))
-logHeadState = pandas.DataFrame(pandas.read_csv('soudlocalizer_dumper/logHeadState/data.log', sep=' ', header=None))
+logHeadState = pandas.DataFrame(pandas.read_csv('soudlocalizer_dumper/logHeadState/data.log',
+                                                sep=' ', header=None, index_col=0))
 
 for file in glob.glob('audioSamples/20200512-202942/*.wav'):
     id_audio = file[29:30]
-    audio_filenames = file[31:49]
+    audio_timestamp = float(file[31:49])
+    indexAngles = abs(logAngles[1] - audio_timestamp).idxmin()
+    indexHead = abs(logHeadState[1] - logAngles.at[indexAngles, 1]).idxmin()
 
-
-
-
-
-
-
-
+    #result = [id_audio, audio_timestamp, logAngles.loc[indexAngles], logHeadState.loc[indexHead]]
 
