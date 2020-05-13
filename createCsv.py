0fc0dc76be49527a5d3e6fee26bc1514e9f36871
Alllessia <41387340+Alllessia@users.noreply.github.com>
2020-05-13 18:39:50 +0200

    Ultimate with result_aux

--- createCsv.py
+++ createCsv.py
@@ -14,8 +14,12 @@ for file in glob.glob('audioSamples/20200512-202942/*.wav'):
     audio_timestamp = float(file[31:49])
     indexAngles = abs(logAngles[1] - audio_timestamp).idxmin()
     indexHead = abs(logHeadState[1] - logAngles.at[indexAngles, 1]).idxmin()
-    result_aux = pandas.DataFrame([[id_audio, audio_timestamp, logAngles.at[indexAngles, 2],
-                                   logHeadState.at[indexHead, 2], logHeadState.at[indexHead, 3],
-                                   logHeadState.at[indexHead, 4], logHeadState.at[indexHead, 5],
-                                   logHeadState.at[indexHead, 6], logHeadState.at[indexHead, 7]]], columns=column_names)
-    result = result.append(result_aux)
+    result_aux = pandas.DataFrame([[id_audio, audio_timestamp, str(logAngles.at[indexAngles, 2]) + " " +
+                                    str(logAngles.at[indexAngles, 3]) + " " + str(logAngles.at[indexAngles, 4]),
+                                    logHeadState.at[indexHead, 2], logHeadState.at[indexHead, 3],
+                                    logHeadState.at[indexHead, 4], logHeadState.at[indexHead, 5],
+                                    logHeadState.at[indexHead, 6], logHeadState.at[indexHead, 7]]],
+                                  columns=column_names)
+    result = result.append(result_aux, ignore_index=True)
+
+result.to_csv('result.csv', index=False)
